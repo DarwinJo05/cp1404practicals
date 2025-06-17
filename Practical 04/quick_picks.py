2ffef2f934ad2a04dabf6a96bc4b5cb8013ce031
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-17 19:56:06 +0800

    Generate quick pick and display quick pick

--- Practical 04/quick_picks.py
+++ Practical 04/quick_picks.py
@@ -7,3 +7,16 @@ def main():
     number_of_picks = int(input("How many quick picks? "))
     for i in range(number_of_picks):
         quick_pick = generate_quick_pick()
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
+def generate_quick_pick():
+    """Generate six random numbers and put them in a list."""
+    numbers = []
+    while len(numbers) < 6:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    return numbers
+
+main()
