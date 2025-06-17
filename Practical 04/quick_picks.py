e916a42ee5b0a8b27b39e2c155fb8ebd37d7c4f5
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-17 19:17:54 +0800

    Add main funtion with user input

--- Practical 04/quick_picks.py
+++ Practical 04/quick_picks.py
@@ -2,3 +2,8 @@ import random
 
 MIN_NUMBER = 1
 MAX_NUMBER = 45
+
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    for i in range(number_of_picks):
+        quick_pick = generate_quick_pick()
