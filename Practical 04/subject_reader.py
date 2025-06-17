3e645c85dd4b95fce9df497bbec70e9e848a70dc
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-17 13:41:29 +0800

    Add function to display subject details

--- Practical 04/subject_reader.py
+++ Practical 04/subject_reader.py
@@ -9,7 +9,8 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
-
+    display_data = display_subject_details(data)
+    print(display_data)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
@@ -21,6 +22,11 @@ def load_data():
         subject_data.append(parts)
     input_file.close()
     return subject_data
+
+def display_subject_details(data):
+    """Display subject details."""
+    for subject in data:
+        print(subject[0], "is taught by", subject[1], "and has", subject[2], "students")
 
 
 main()
