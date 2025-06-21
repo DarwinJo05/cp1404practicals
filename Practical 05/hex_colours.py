9bbfd1160b5ad0c916ec572747915f7cb93c8c36
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-22 03:14:15 +0800

    User input and check error

--- Practical 05/hex_colours.py
+++ Practical 05/hex_colours.py
@@ -1,12 +1,20 @@
 COLOUR_NAME_TO_HEX = {
-    "AliceBlue": "#f0f8ff",
-    "AntiqueWhite": "#faebd7",
-    "Aqua": "#00ffff",
-    "Aquamarine": "#7fffd4",
-    "Azure": "#f0ffff",
-    "Beige": "#f5f5dc",
-    "Bisque": "#ffe4c4",
-    "Black": "#000000",
-    "Blue": "#0000ff",
-    "Coral": "#FF7F50"
+    "aliceBlue": "#f0f8ff",
+    "antiqueWhite": "#faebd7",
+    "aqua": "#00ffff",
+    "aquamarine": "#7fffd4",
+    "azure": "#f0ffff",
+    "beige": "#f5f5dc",
+    "bisque": "#ffe4c4",
+    "black": "#000000",
+    "blue": "#0000ff",
+    "coral": "#FF7F50"
 }
+
+colour_name = input("Enter colour: ").lower()
+while colour_name != "":
+    try:
+        print(f"{colour_name.title()} is {COLOUR_NAME_TO_HEX[colour_name]}")
+    except KeyError:
+        print("Invalid colour name")
+    colour_name = input("Enter colour: ").lower()
