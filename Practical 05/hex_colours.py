17cb193184671ec159a19c2b5c85bfb835d8b42d
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-22 02:10:01 +0800

    Add constant dictionary of colour names to hex

---
+++ Practical 05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOUR_NAME_TO_HEX = {
+    "AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Aquamarine": "#7fffd4",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "Blue": "#0000ff",
+    "Coral": "#FF7F50"
+}
