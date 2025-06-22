57dfddcdfcb0cd2ef4cf70f1eec83aec0485d697
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-22 18:22:45 +0800

    Get list of champion countries

--- Practical 05/wimbledon.py
+++ Practical 05/wimbledon.py
@@ -8,11 +8,12 @@ FILENAME = "wimbledon.csv"
 def main():
     data = read_wimbledon_data(FILENAME)
     champion_to_count = count_champions(data)
+    countries = get_champion_countries(data)
 
 
 def read_wimbledon_data(FILENAME):
     """Read file insert into list"""
-    with open(FILENAME, "r") as in_file:
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         data = [line.strip().split(",") for line in in_file]
     return data
@@ -27,5 +28,13 @@ def count_champions(data):
         else:
             champion_to_count[champion] = 1
     return champion_to_count
+
+def get_champion_countries(data):
+    """Get set of unique countries of champions"""
+    countries = set()
+    for row in data:
+        country = row[1]
+        countries.add(country)
+    return countries
 
 main()
