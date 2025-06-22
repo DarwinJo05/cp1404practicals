931bd81f54332f4cdedf60945efc6ba77303d6d1
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-22 17:13:52 +0800

    Count the number of times each champion has won

--- Practical 05/wimbledon.py
+++ Practical 05/wimbledon.py
@@ -7,10 +7,25 @@ FILENAME = "wimbledon.csv"
 
 def main():
     data = read_wimbledon_data(FILENAME)
+    champion_to_count = count_champions(data)
+
 
 def read_wimbledon_data(FILENAME):
+    """Read file insert into list"""
     with open(FILENAME, "r") as in_file:
         in_file.readline()
         data = [line.strip().split(",") for line in in_file]
     return data
 
+def count_champions(data):
+    """Count how many times each champion has won"""
+    champion_to_count = {}
+    for row in data:
+        champion = row[2]
+        if champion in champion_to_count:
+            champion_to_count[champion] += 1
+        else:
+            champion_to_count[champion] = 1
+    return champion_to_count
+
+main()
