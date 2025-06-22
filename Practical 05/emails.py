a4c93969c9df4f1c4cc26c6ef590ecf579ba88a8
darwin <darwin.jonathan@my.jcu.edu.au>
2025-06-22 12:08:23 +0800

    Main function for user input

--- Practical 05/emails.py
+++ Practical 05/emails.py
@@ -3,3 +3,12 @@ Emails
 Estimated: 30 minutes
 Actual:
 """
+
+def main():
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        name = extract_name(email)
+
+def extract_name(email):
