3e51a9836b3955e4b139b852dbdf049894587743
darwin <darwin.jonathan@my.jcu.edu.au>
2025-07-09 16:12:08 +0800

    Add ProgrammingLanguage class with initializer

---
+++ prac_06/programming_language.py
@@ -0,0 +1,7 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name = "", typing = "", is_reflection = False, year = 0):
+        self.name = name
+        self.typing = typing
+        self.is_Reflection = is_reflection
+        self.year = year
