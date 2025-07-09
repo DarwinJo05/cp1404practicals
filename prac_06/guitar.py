1ec20383cc8a568e0ac54b84bb12bfacc21744bc
darwin <darwin.jonathan@my.jcu.edu.au>
2025-07-09 17:30:40 +0800

    Add Guitar class and initializer

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+class Guitar:
+    """Class of Guitar"""
+
+    def __init__(self, name= "", year=0, cost=0):
+        """Initialise Guitar with name, year, and cost."""
+        self.name = name
+        self.year = year
+        self.cost = cost
