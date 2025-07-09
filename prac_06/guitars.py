eb9061cb97b2a4ef0c36c291beefae48fedaf9ad
darwin <darwin.jonathan@my.jcu.edu.au>
2025-07-09 19:44:24 +0800

    Get year and cost input, followed by display and the import code

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,8 +1,16 @@
+from prac_06.guitar import Guitar
+
 def main():
     guitars = []
 
     print("My guitars!")
     name = input("Name: ")
     while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar}, added.")
+        name = input("Name: ")
 
 main()
