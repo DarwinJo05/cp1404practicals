67f2c80c7320894d5f92f5a02343ad3a07981873
darwin <darwin.jonathan@my.jcu.edu.au>
2025-07-09 18:18:08 +0800

    Add __str__ method for formatted string output

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,3 +6,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string in the format"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
