e1ce86b398a8552fec3408b250e78d1f65feff25
darwin <darwin.jonathan@my.jcu.edu.au>
2025-07-16 19:19:10 +0800

    Add user input, sorting, and saving guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,6 +7,12 @@ def main():
     print("These are the guitars loaded:")
     display_guitars(guitars)
     guitars.extend(get_new_guitars())
+    guitars.sort()
+    print("These are the guitars after adding and sorting:")
+    display_guitars(guitars)
+
+    save_guitars(FILENAME, guitars)
+    print(f"{len(guitars)} guitars saved to {FILENAME}")
 
 def load_guitars(filename):
     guitars = []
@@ -25,7 +31,7 @@ def display_guitars(guitars):
 
 def get_new_guitars():
     new_guitars = []
-    print("Enter new guitars :")
+    print("\nEnter new guitars (leave name blank to finish):")
     name = input("Name: ")
     while name != "":
         try:
@@ -36,6 +42,11 @@ def get_new_guitars():
             print("Invalid input. Try again.")
         name = input("Name: ")
     return new_guitars
+
+def save_guitars(filename, guitars):
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 if __name__ == "__main__":
     main()
