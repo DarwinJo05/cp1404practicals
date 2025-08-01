b7710e6daa63722c05a87203e4632b6621e7d8b3
darwin <darwin.jonathan@my.jcu.edu.au>
2025-08-01 23:02:42 +0800

    Create taxi simulator main program with menu loop and Taxi imports

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,17 @@
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    print("Let's drive!")
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    current_taxi = None
+
+    total_cost = 0
+
+if __name__ == '__main__':
+main()
