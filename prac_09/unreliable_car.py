70346985a36834cfbd5343dd54b7360b70f138c5
darwin <darwin.jonathan@my.jcu.edu.au>
2025-08-01 12:24:06 +0800

    Make drive only work sometimes using chance.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,12 @@ class UnreliableCar(Car):
         """Initialise the car with name, fuel and reliability."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Attempt to drive the car based on reliability.
+        Drive only if a random number is less than reliability.
+        """
+        if random.uniform(0, 100) < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
